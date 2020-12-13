698f1cfc51212df3aa4cb74d4b09377fee18eda5
lulu <3431879181qq.com>
2020-12-13 16:35:27 +0800

    添加上传功能

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -1,8 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render
+from .models import Student, Homework
+from django.views.generic.edit import CreateView
 
-from .models import Article
+'''def year_archive(request, year):
+    a_list = Article.objects.filter(pub_date__year=year)
+    context = {'year': year, 'article_list': a_list}
+    return render(request, 'news/year_archive.html', context)'''
 
-def year_archive(request, year):
-    a_list = Article.objects.filter(pub_date__year=year)
-    context = {'year': year, 'article_list': a_list}
-    return render(request, 'news/year_archive.html', context)
+class HomeworkCreate(CreateView):
+    model = Homework
+    template_name = 'homework_form.html'
+    fields = ['headline','attach','remark', 'student']
